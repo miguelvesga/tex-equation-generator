09cf6a98383e8860a0c504f3dd223268f9053a99
Miguel Vesga <miguelvesga@outlook.com>
2017-06-30 10:25:23 -0500

    MathJax CDN has been shut down as of April 30, 2017.

--- js/content.js
+++ js/content.js
@@ -78,7 +78,7 @@ window.MathJax = {
   script.onload = function() {
     // remote script has loaded
   };
-  script.src = 'https://cdn.mathjax.org/mathjax/latest/MathJax.js';
+  script.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js';
   d.getElementsByTagName('head')[0].appendChild(script);
 
 }(document));
